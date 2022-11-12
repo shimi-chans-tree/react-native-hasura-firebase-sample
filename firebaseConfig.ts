0ae305cd400063eda3db449ef165a737546680dc
koji <koji@kojinoMacBook-Pro.local>
2022-11-13 07:53:31 +0900

    firebaseConfigの追加

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+
+const config = {
+  apiKey: Constants.expoConfig.extra.apiKey,
+  authDomain: Constants.expoConfig.extra.authDomain,
+  projectId: Constants.expoConfig.extra.projectId,
+  storageBucket: Constants.expoConfig.extra.storageBucket,
+  messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
+  appId: Constants.expoConfig.extra.appId,
+};
+
+const firebaseApp = !getApps().length ? initializeApp(config) : getApp();
+export const auth = getAuth();
+export const db = getFirestore();
